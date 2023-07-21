3753d163f6c27111f05465ab5e84d551caa6d5f8
jrilett <james.rilett@gmail.com>
2023-07-21 21:03:53 +0100

    Update

--- scripts/site_scraper.py
+++ scripts/site_scraper.py
@@ -8,6 +8,9 @@ from selenium import webdriver
 from webdriver_manager.firefox import DriverManager
 from time import time, sleep
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pickle
 
 # Specify the path to geckodriver
@@ -20,21 +23,25 @@ driver = webdriver.Firefox(service=service)
 # input URL - we should have a list to iterate through but the sites may have different layouts from one another
 url = "https://www.avisonyoung.co.uk/properties/-/property/results?industrial=1&south_west=1&units=sqft&size_min_sqft=-1&size_min_sqm=-1&size_min_acres=-1&size_max_sqft=0&size_max_sqm=0&size_max_acres=0&submit=SEARCH&type=r"
 cookies = pickle.load(open("scripts/cookies.pkl", "rb"))
-driver.get(url)
 for cookie in cookies:
     driver.add_cookie(cookie)
+driver.get(url)
+try:
+    driver.find_element(By.ID, 'hs-eu-confirmation-button').click()
+except:
+    pass
 # driver opens the browers on yr pc 
 
 
 soup = BeautifulSoup(driver.page_source, 'html')
 grid_item = soup.find_all("section", class_='grid-item')
 
-property_url_list = [x.find('a')["href"] for x in grid_item] 
+property_url_list = [x.find('a')["href"] for x in grid_item]
 property_info = []
 
 for p_url in property_url_list : 
 
-    driver.get(property_url_list)
+    driver.get(p_url)
     property_soup = BeautifulSoup(driver.page_source, 'html')
     property_type = 1 if 'industrial' in url.lower() else 0
     
@@ -59,16 +66,13 @@ for p_url in property_url_list :
     except:
         agent_number = ''
 
-    property_info.append([p_url, property_address, property_cost, property_size_sqft, property_size_sqm])
+    property_info.append([p_url, property_type, property_address, property_cost, property_size_sqft, property_size_sqm, agent_number])
     sleep(5)
     print(p_url)
 
-pd.DataFrame(property_info, columns = ['url','address', 'cost', 'size_sqft', 'size_sqm','agent_number']).to_csv("first_db.csv")
+pd.DataFrame(property_info, columns = ['url', 'property_type', 'address', 'cost', 'size_sqft', 'size_sqm','agent_number']).to_csv("first_db.csv")
 
-
-
-
-
+'''
 # this scrolls to the bottom of the page
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -81,4 +85,6 @@ soup = BeautifulSoup(driver.page_source, 'lxml')
 # need to iterate through the page items to get the info
 
 # example of find all list items on the page with the class name
-#soup.find_all('li', class_="quote-news-headlines__item")[0].find('a')['href']
+soup.find_all('li', class_="quote-news-headlines__item")[0].find('a')['href']
+
+'''
